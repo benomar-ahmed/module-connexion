a092c25e4dd873ed0b80b41a1c53c1148e8603b9
benomar-ahmed <114390073+benomar-ahmed@users.noreply.github.com>
2022-12-07 16:22:31 +0100

    set a link to go project github

--- moduleconnexion/admin.php
+++ moduleconnexion/admin.php
@@ -61,5 +61,8 @@ $row = $result->fetch_all();
             </table>
         </div>
     </main>
+    <footer>
+        <a href="https://github.com/benomar-ahmed/module-connexion.git"><img src="../Images/icon-github.png" alt="Le logo de github"></a>
+    </footer>
 </body>
 </html>
